a98f6854be44162591a626821682faa7f4976aca
Haruka Teduka <th_kiyose@outlook.com>
2020-06-03 14:54:20 +0900

    confirmのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :properties, only: %i(index show new create edit update destroy)
+  resources :properties, only: %i(index show new create edit update destroy) do
+    collection do
+      post 'confirm'
+    end
+  end
   root to: 'properties#index'
 end
