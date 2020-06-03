6e240f9a631dcc581b1f276a351a737958419aa1
Haruka Teduka <th_kiyose@outlook.com>
2020-06-03 10:43:26 +0900

    new,createアクション定義

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -4,11 +4,26 @@ class PropertiesController < ApplicationController
     @properties = Property.all.order(created_at: :ASC)
   end
 
+  def new
+    @property = Property.new
+  end
+
+  def create
+    property = Property.new(get_params)
+    if property.save
+      redirect_to properties_path, notice: "物件を登録しました。"
+    end
+  end
+
   def show
   end
 
   private
   def set_property
     @property = Property.find_by(id: params[:id])
+  end
+
+  def get_params
+    params.require(:property).permit(:name,:rent,:place,:old,:comment)
   end
 end
