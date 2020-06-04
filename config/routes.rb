b3f368839bf7b8b7860b26da8368109b87369476
Haruka Teduka <th_kiyose@outlook.com>
2020-06-04 21:18:37 +0900

    rootのルーティングを一番上に移動

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'properties#index'
   resources :properties, only: %i(index show new create edit update destroy) do
     collection do
       post 'confirm'
     end
   end
-  root to: 'properties#index'
 end
