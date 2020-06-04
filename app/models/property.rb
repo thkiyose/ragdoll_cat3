16a4177d5002ee820386e4b0867b4e0032456256
Haruka Teduka <th_kiyose@outlook.com>
2020-06-04 12:43:01 +0900

    アソシエーションの設定

--- app/models/property.rb
+++ app/models/property.rb
@@ -4,4 +4,6 @@ class Property < ApplicationRecord
   validates :place, presence:true, length:{ maximum: 45 }
   validates :old, presence:true, numericality:{ only_integer:true, greater_than_or_equal_to: 0 }, length:{ maximum: 3 }
   validates :comment, length:{ maximum: 255 }
+  has_many :stations, dependent: :destroy
+  accepts_nested_attributes_for :stations
 end
