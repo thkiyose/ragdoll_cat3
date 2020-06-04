188e6660abc0a588ac0a94168d41227ebb2fea31
Haruka Teduka <th_kiyose@outlook.com>
2020-06-04 20:24:48 +0900

    stationモデルにバリデーション記述

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,3 +1,6 @@
 class Station < ApplicationRecord
   belongs_to :property
+  validates :line_near, length:{ maximum: 20 }
+  validates :station_near, length: { maximum: 30 }
+  validates :minutes_needed, numericality:{ only_integer:true, greater_than_or_equal_to: 0 }, length:{ maximum: 4 }
 end
